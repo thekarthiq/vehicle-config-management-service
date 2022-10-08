63c418b61c691ce15ba3ecd82d17a4ea69c7417b
Karthik Vignesh <thekarthiq@gmail.com>
2022-10-08 16:32:09 +0530

    first commit - includes basic configuration of the config service offered by spring cloud.

---
+++ src/test/java/com/eden/management/vehicle/configmanagementservice/ConfigManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eden.management.vehicle.configmanagementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
